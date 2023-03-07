9b04e7b96b05388f71a14c2dc8155080f169c256
Lorens Gueron <gueronlj@Lorenss-MacBook-Pro.local>
2023-03-07 13:18:29 -0500

    wage report now done on back-end

--- controllers/report_controller.js
+++ controllers/report_controller.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const report = express.Router()
 const Employee = require('../models/employee.js')
+const {differenceInHours, parseISO} = require('date-fns');
+
 //----------Create report of shifts in between 2 given dates
 report.get('/:id/:start/:end',(req, res) => {
    Employee.findById(req.params.id,(error, data) => {
       startLimit = (req.params.start)
       endLimit = (req.params.end)
+
       if(error){
          res.json(error)
       }else{
@@ -13,7 +16,25 @@ report.get('/:id/:start/:end',(req, res) => {
             //if shift.date is in between filterStart and filterEnd, map to new array
             shift.date>=startLimit && shift.date<=endLimit
          ))
-         res.json(filteredList)
+         let totalHours = 0
+         let totalDailyWages = data.perDiem * filteredList.length
+         let totalHourlyWages = data.perHour * totalHours
+         filteredList.forEach((shift) => {
+           if(shift.start && shift.end){
+              let startTime = parseISO(shift.start)
+              let endTime = parseISO(shift.end)
+              let hoursInShift = differenceInHours(endTime, startTime)
+              totalHours += hoursInShift
+           }
+         })
+         console.log(totalDailyWages, totalHourlyWages, data.perHour);
+         res.json({
+           schedule:filteredList,
+           totalDays:filteredList.length,
+           totalHours: totalHours,
+           totalDailyWages: totalDailyWages,
+           totalHourlyWages: totalHours * data.perHour
+         })
       }
    })
 })
